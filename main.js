d0837473d8ec75fe6c200aa811a9d8fc28db2f13
Aaron Pang <pykm05@gmail.com>
2024-10-11 16:16:44 -0700

    basic cube rotation

--- main.js
+++ main.js
@@ -13,7 +13,7 @@ const camera = new THREE.PerspectiveCamera( 70, window.innerWidth / window.inner
 // adjusts window size of the scene
 const renderer = new THREE.WebGLRenderer();
 renderer.setSize(window.innerWidth, window.innerHeight );
-renderer.setAnimationLoop( animate );
+// renderer.setAnimationLoop( animate );
 document.body.appendChild( renderer.domElement );
 
 const geometry = new THREE.BoxGeometry( 1, 1, 1 );
@@ -23,23 +23,59 @@ scene.add( cube );
 
 camera.position.z = 3;
 
-function animate() {
 
-	cube.rotation.x += 0.01;
-	cube.rotation.y += 0.01;
+// function animate() {
 
-	renderer.render( scene, camera );
-}
+// 	cube.rotation.x += 0.01;
+// 	cube.rotation.y += 0.01;
+
+// 	renderer.render( scene, camera );
+// }
 
 // Device and browser compatibility check for WebGL 2
 if ( WebGL.isWebGL2Available() ) {
 
 	// Initiate function or other initializations here
-	renderer.setAnimationLoop(animate);
+	renderer.render(scene, camera);
 
 } else {
 
 	const warning = WebGL.getWebGL2ErrorMessage();
 	console.log(warning);
 
+}
+
+document.addEventListener('keydown', (event) => {
+	const keyName = event.key;
+	const rotationSpeed = -0.5;
+	switch (keyName) {
+		case ';': 
+			cubeRotate('y', -1);
+			break;
+		case 'a':
+			cubeRotate('y', 1);
+			break;
+		case 'y':
+			cubeRotate('x', 1);
+			break;
+		case 'b':
+			cubeRotate('x', -1);
+			break;
+	}
+});
+
+const cubeRotate = (dimension, direction) => {
+
+	let currRotate = 0;
+	const maxRotate = Math.PI / 2;
+	const rotationSpeed = 0.2 * direction;
+
+	const timer = setInterval(() => {
+		currRotate += rotationSpeed;
+		cube.rotation[dimension] += rotationSpeed;
+		renderer.render(scene, camera);
+		if (Math.abs(currRotate) >= maxRotate) {
+			clearInterval(timer);
+		}
+	}, 100);
 }
