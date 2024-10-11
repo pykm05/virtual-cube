baf627f2ff3113d9f491cd2724b93167065bd7ec
Aaron Pang <pykm05@gmail.com>
2024-10-10 23:39:43 -0700

    draw 2d arrow in test.js

--- main.js
+++ main.js
@@ -7,10 +7,12 @@ const scene = new THREE.Scene();
 
 // Camera display settings
 // params: FOV(field of wiew, angle of view), aspect ratio(think old TVs), near and far clipping plane
+// adjusts angle of view
 const camera = new THREE.PerspectiveCamera( 70, window.innerWidth / window.innerHeight, 0.1, 1000 );
 
+// adjusts window size of the scene
 const renderer = new THREE.WebGLRenderer();
-renderer.setSize( window.innerWidth, window.innerHeight );
+renderer.setSize(window.innerWidth, window.innerHeight );
 renderer.setAnimationLoop( animate );
 document.body.appendChild( renderer.domElement );
 
@@ -19,7 +21,7 @@ const material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
 const cube = new THREE.Mesh( geometry, material );
 scene.add( cube );
 
-camera.position.z = 5;
+camera.position.z = 3;
 
 function animate() {
 
