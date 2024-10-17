b5d360a8d803fd551e10d78e4b61691fd6a1bce1
Aaron Pang <pykm05@gmail.com>
2024-10-16 23:10:22 -0700

    create whole cube and modified cube class to store layer groups

--- cube.js
+++ cube.js
@@ -4,42 +4,30 @@ export class Cube {
 	pieces = [];
   groups = [];
 
+  Full_Cube = new THREE.Group();
 	U_Layer = new THREE.Group();
 	F_Layer = new THREE.Group();
 
   // temporary single-layer cube
   constructor() {
-    this.pieces.push(this.Piece(-1, 0, -1));
-    this.pieces.push(this.Piece(0, 0, 0));
-    this.pieces.push(this.Piece(0, 0, -1));
-    this.pieces.push(this.Piece(1, 0, -1));
-    this.pieces.push(this.Piece(-1, 0, 0));
-    this.pieces.push(this.Piece(1, 0, 0));
-    this.pieces.push(this.Piece(-1, 0, 1));
-    this.pieces.push(this.Piece(0, 0, 1));
-    this.pieces.push(this.Piece(1, 0, 1));
+    for (let i = -1; i < 2; i++) {
+      for (let j = -1; j < 2; j++) {
+        for (let k = -1; k < 2; k++) {
+          this.pieces.push(this.Piece(i, j ,k));
+        }
+      
+      }
+    }
 
     this.groups.push(this.U_Layer);
     this.groups.push(this.F_Layer);
+    this.groups.push(this.Full_Cube);
   };
-
-	createCube(x, y, z) {
-		if (x == 1 || y == 1 || z == 1) {
-			this.pieces.add(Piece(x, y, z));
-			return;
-		}
-	}
 
 	Piece(x, y, z) {
 		const geometry = new THREE.BoxGeometry( 1, 1, 1 );
 		const material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
 		const piece = new THREE.Mesh( geometry, material );
-    if (y == 0) {
-      this.U_Layer.add(piece)
-    }
-    if (z == 1) {
-      this.F_Layer.add(piece);
-    }
 
 		piece.position.set(x, y, z);
 		return piece;
