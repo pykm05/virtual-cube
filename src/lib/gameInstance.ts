7df1f5391fd1c0734217e78b0ce67362e076931f
Aaron Pang <pykm05@gmail.com>
2025-06-05 15:06:44 -0700

    update cube socket listening logic

--- src/lib/gameInstance.ts
+++ src/lib/gameInstance.ts
@@ -2,22 +2,16 @@ import { getSocket } from "@/lib/socket";
 import { Cube } from "../components/three/cube";
 import Scene from "../components/three/scene";
 
-export default function newScene(container: HTMLElement, socketID: string) {
-  if (container.hasChildNodes()) return;
-
+export default function newScene(container: HTMLElement, assignedSocketID: string) {
   const { scene, renderer, camera } = Scene(container);
   const cube = new Cube(scene, renderer, camera);
 
   const socket = getSocket();
-  const notOpponentScene = socket.id != socketID;
+  const emitter = socket.id == assignedSocketID;
 
-  if (notOpponentScene) {
-    window.addEventListener('keydown', (e) => socket.emit("keyboard input", socket.id, e.key));
-  }
+  if (emitter) window.addEventListener('keydown', (e) => socket.emit("keyboard input", socket.id, e.key));
 
   socket.on("keyboard input", (socketID: string, key: string) => {
-    if ((!notOpponentScene && socketID != socket.id) || (notOpponentScene && socketID == socket.id)) cube.handleInput(key);
+    if ((emitter && socket.id == socketID) || (!emitter && assignedSocketID == socketID)) cube.handleInput(key);
   });
-
-  renderer.render(scene, camera);
 }
