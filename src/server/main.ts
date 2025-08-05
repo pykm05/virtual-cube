3284008d8da14fce0fc4721eb15443c0d50510fc
Bowarc <yuyeaaaahhhh@laposte.net>
2025-08-05 07:21:12 +0200

    Cleanup

--- src/server/main.ts
+++ src/server/main.ts
@@ -2,7 +2,6 @@ import express from 'express';
 import cors from 'cors';
 import http from 'http';
 import './modules/deps.ts';
-import { test } from './db.ts';
 
 const app = express();
 const port = 4000;
@@ -21,4 +20,3 @@ server.listen(port, () => {
     deps['webSocket'].init(server);
 });
 
-await test();
