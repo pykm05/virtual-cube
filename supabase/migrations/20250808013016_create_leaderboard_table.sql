8784939a88fe185819b6cd6337e4c2b444e50b6f
Aaron Pang <pykm05@gmail.com>
2025-08-09 14:18:02 -0700

    Define new column id as primary key for leaderboard

--- supabase/migrations/20250808013016_create_leaderboard_table.sql
+++ supabase/migrations/20250808013016_create_leaderboard_table.sql
@@ -1,4 +1,5 @@
 create table leaderboard (
+  id uuid PRIMARY KEY DEFAULT gen_random_uuid();
   username TEXT NOT NULL,
   solve_duration FLOAT NOT NULL,
   solved_at TIMESTAMPTZ NOT NULL DEFAULT now()
